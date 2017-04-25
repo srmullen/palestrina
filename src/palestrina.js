9b042511d9734803c05416d16b10f1bdc2dff393
Sean Mullen <srmullen13@gmail.com>
2017-04-25 10:02:39 -0600

    export pitch. Don't set on window

--- src/palestrina.js
+++ src/palestrina.js
@@ -3,10 +3,7 @@
 import * as scale from "./scale";
 import * as melody from "./melody";
 import * as chord from "./chord";
+import * as pitch from "./pitch";
 import * as live from "./live";
 
 export {scale, melody, chord, live};
-
-if (typeof window === "object") {
-	window.palestrina = {scale, melody, chord, live};
-}
